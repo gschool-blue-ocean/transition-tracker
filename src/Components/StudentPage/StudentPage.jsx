ea20d3367e27323c5059f94d47002a29c26c3c2b
AnthonyWright17 <awright032317@gmail.com>
2022-07-21 16:09:21 -0600

    Listed items

--- src/Components/StudentPage/StudentPage.jsx
+++ src/Components/StudentPage/StudentPage.jsx
@@ -7,58 +7,82 @@ export default function StudentPage() {
       <div className="StudentDash--Wrapper">
          <div className="SDash--Header">
             <h3>Anthony Wright</h3>
-            <div>MCSP-12</div>
-            <div>ETS'd</div>
+            <p>MCSP-12</p>
+            <p>ETS'd</p>
          </div>
 
-         <div className="SDash--Info">
-            <div>
-               <h4>Personal Info</h4>
-               <div>Email: awright@gmail.com</div>
-               <div>Branch: US Army</div>
-               <div>MOS: 31B</div>
-               <div>Duty Station: Fort Bliss</div>
-               <div>Terminal Leave: 07/10/22</div>
-               <div>ETS: 07/12/22</div>
-               <div>
-                  <label>TAPs: </label>
-                  <select>
-                     <option value="Not Started">Not Started</option>
-                     <option value="In Progress">In Progress</option>
-                     <option value="Completed">Completed</option>
-                  </select>
-               </div>
+         {/* User Data Card */}
+         <div className="SDash--Info-card">
 
-               <h4>Dependents</h4>
-               <select>
-                  <option value="No">No</option>
-                  <option value="Yes">Yes</option>
-               </select>
+            <div className="infoCard--container">
+               <ul>
 
-               <h4>Education</h4>
-               <div>Degree: None</div>
-               <div>
-                  <label>Pursuing Degree: </label>
-                  <select>
-                     <option value="No">No</option>
-                     <option value="Yes">Yes</option>
-                  </select>
-               </div>
+               <h4 className="text-left">Personal Info</h4>
 
-               <h4>Relocation</h4>
-               <div>
-                  <label>Planning to Relocation: </label>
-                  <select>
-                     <option value="No">No</option>
-                     <option value="Yes">Yes</option>
-                  </select>
-               </div>
-               <label>Preferred Location</label>
-            </div>
+                  <li> 
+                     <span className="title"> Email: </span>
+                     <span className="answer"> awright@gmail.com </span>
+                  </li>
+
+                  <li> 
+                     <span className="title"> Branch: </span>
+                     <span className="answer"> Army </span>
+                  </li>
+
+                  <li> 
+                     <span className="title under-line"> MOS: </span>
+                     <span className="answer"> Answer </span>
+                  </li>
+
+                  <li> 
+                     <span className="title"> Duty Station: </span>
+                     <span className="answer"> Answer </span>
+                  </li>
+
+                  <li> 
+                     <span className="title"> Terminal Leave: </span>
+                     <span className="answer"> Answer </span>
+                  </li>
+
+                  <li> 
+                     <span className="title"> ETS: </span>
+                     <span className="answer"> Answer </span>
+                  </li>
+
+                  <li> 
+                     <span className="title"> Title: </span>
+                     <span className="answer"> Answer </span>
+                  </li>
+
+                  <li>
+                     <h4 id="depends" className="text-left">Dependents</h4>
+                     <span className="title">None</span>
+                  </li>
+
+                  <li>
+                     <span className="title">TAP Status: </span>
+                     <span className="answer">Completed</span>
+                  </li>
+
+                  <li>
+                     <h4 className="text-left">Education</h4>
+                     <span className="title">Degree: Hell Naw</span>
+                  </li>
+
+                  <li>
+                     <span className="title">Interest in pursing a degree</span>
+                  </li>
+
+                  <li>
+                     <h4 className="text-left">Relocation</h4>
+                     <span className="title">Planning to Rellocate?</span>
+                     <span className="answer"> Yes</span>
+                  </li>
+
+               </ul>
+            </div>  
+               
          </div>
-
-         <div className="SDash--Tasks"></div>
-         <div className="SDash--Notes"></div>
-      </div>
+      </div>  
    );
 }
