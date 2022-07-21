a4148a199d965002732dd28cb4654b10b2392488
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-21 14:10:09 -0500

    Initial Format/Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,52 @@
-import './StyleSheets/App.css';
-import { useState, useContext, useEffect } from 'react';
-import { Routes, Route } from "react-router-dom"
-import Loading from './Components/LoadingDisplay/Loading'
-import LandingPage from './Components/Landing/LandingPage'
-import HomePage from './Components/HomePage/HomePage'
-import CreateAccountModal from './Components/Landing/CreateAccountModal';
-import LoginContext from './Context/LoginContext';
-import AppContext from './Context/AppContext';
-import "./StyleSheets/Header.css"
-import StudentPage from './Components/StudentPage/StudentPage';
+import "./StyleSheets/App.css";
+import { useState, useContext, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Loading from "./Components/LoadingDisplay/Loading";
+import LandingPage from "./Components/Landing/LandingPage";
+import HomePage from "./Components/HomePage/HomePage";
+import CreateAccountModal from "./Components/Landing/CreateAccountModal";
+import LoginContext from "./Context/LoginContext";
+import AppContext from "./Context/AppContext";
+import "./StyleSheets/Header.css";
+import StudentPage from "./Components/StudentPage/StudentPage";
 
 function App() {
-  const { login, userData, invokeSetLogin, loading } = useContext(LoginContext)
-  const { allUsersData, allCohortsData, invokeSetAllUsersData, invokeSetAllCohortsData } = useContext(AppContext)
+   const { login, userData, invokeSetLogin, loading } = useContext(LoginContext);
+   const { allUsersData, allCohortsData, invokeSetAllUsersData, invokeSetAllCohortsData } = useContext(AppContext);
 
-  useEffect(() => {
-    fetchAllUserData()
-    fetchAllCohortData()
-  }, [])
+   useEffect(() => {
+      fetchAllUserData();
+      fetchAllCohortData();
+   }, []);
 
-  const fetchAllUserData = () => {
-    fetch('https://hacking-transition.herokuapp.com/api/users')
-      .then(res => res.json())
-      .then(data => invokeSetAllUsersData(data))
-      .catch(err => console.log(err))
-  }
-  const fetchAllCohortData = () => {
-    fetch('https://hacking-transition.herokuapp.com/api/cohorts')
-      .then(res => res.json())
-      .then(data => invokeSetAllCohortsData(data))
-      .catch(err => console.log(err))
-  }
+   const fetchAllUserData = () => {
+      fetch("https://hacking-transition.herokuapp.com/api/users")
+         .then((res) => res.json())
+         .then((data) => invokeSetAllUsersData(data))
+         .catch((err) => console.log(err));
+   };
+   const fetchAllCohortData = () => {
+      fetch("https://hacking-transition.herokuapp.com/api/cohorts")
+         .then((res) => res.json())
+         .then((data) => invokeSetAllCohortsData(data))
+         .catch((err) => console.log(err));
+   };
 
-  // if (!login) {
-  //   if (loading) {
-  //     return (<Loading />)
-  //   }
-  //   return <LandingPage invokeSetLogin={invokeSetLogin} />
-  // }
+   // if (!login) {
+   //   if (loading) {
+   //     return (<Loading />)
+   //   }
+   //   return <LandingPage invokeSetLogin={invokeSetLogin} />
+   // }
 
-  return (
-
-    <div className="AppContainer">
-      <Routes>
-        <Route path="/" element={login ? <StudentPage /> : <LandingPage invokeSetLogin={invokeSetLogin} />} />
-        <Route path="/createAccount" element={<CreateAccountModal />} />
-      </Routes>
-
-
-
-
-    </div>
-  );
+   return (
+      <div className="AppContainer">
+         <Routes>
+            <Route path="/" element={login ? <StudentPage /> : <LandingPage invokeSetLogin={invokeSetLogin} />} />
+            <Route path="/createAccount" element={<CreateAccountModal />} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
