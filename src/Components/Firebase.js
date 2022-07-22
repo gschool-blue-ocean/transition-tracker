f27250fc03171a205db4a054ae0ed0e177207660
Neo Temores <neo@Mac.lan1>
2022-07-22 11:15:06 -0700

    deploy

--- src/Components/Firebase.js
+++ src/Components/Firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,9 +21,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export const auth = getAuth(app);
-app.auth().setPersistence(app.auth.Auth.Persistence.NONE)
+// app.auth().setPersistence(app.auth.Auth.Persistence.NONE)
 
-export { createUserWithEmailAndPassword };
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
 
 // export defaultAuth;
 
--- src/Components/Landing/LoginModal.jsx
+++ src/Components/Landing/LoginModal.jsx
@@ -21,14 +21,15 @@ function LoginModal({ invokeSetLogin, setShowLoginModal }) {
     let navigate = useNavigate()
     const validateUserLoginData = () => {
 
-        allUsersData.forEach((elem) => {
-            if (loginData.username === elem.username && loginData.password === elem.password) {
-                elem.new_user ? navigate("/createAccount") : invokeSetLogin(true)
-                return console.log(elem)
-            }
-        });
+        // allUsersData.forEach((elem) => {
+        //     if (loginData.username === elem.username && loginData.password === elem.password) {
+        //         elem.new_user ? navigate("/createAccount") : invokeSetLogin(true)
+        //         return console.log(elem)
+        //     }
+        // });
 
-        return console.warn('failed login attempt')
+
+        // return console.warn('failed login attempt')
         // invokeSetLogin(true)
     }
     const handleChange = (e) => {
