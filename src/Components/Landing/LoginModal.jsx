19ed0b9ae70503717a60475062951c9a06706344
Neo Temores <neo@Mac.lan1>
2022-07-22 11:20:42 -0700

    deploy

--- src/Components/Landing/LoginModal.jsx
+++ src/Components/Landing/LoginModal.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { CgEnter } from 'react-icons/cg'
 import AppContext from '../../Context/AppContext';
 import { useNavigate } from 'react-router-dom'
+import { auth, signInWithEmailAndPassword } from '../Firebase'
 
 
 function LoginModal({ invokeSetLogin, setShowLoginModal }) {
@@ -20,7 +21,21 @@ function LoginModal({ invokeSetLogin, setShowLoginModal }) {
 
     let navigate = useNavigate()
     const validateUserLoginData = () => {
-
+        signInWithEmailAndPassword(loginData.username, loginData.password)
+            .then(({ user }) => {
+                return user.getIdToken().then((idToken) => {
+                    return fetch(`${window.location.origin}/api/create/user`, {
+                        method: "POST",
+                        headers: {
+                            Accept: "application/json",
+                            "Content-type": "application/json",
+                            "CSRF-Token": Cookies.get("XSRF-TOKEN"),
+                        },
+                        body: JSON.stringify({ idToken })
+                    })
+                }
+                )
+            })
         // allUsersData.forEach((elem) => {
         //     if (loginData.username === elem.username && loginData.password === elem.password) {
         //         elem.new_user ? navigate("/createAccount") : invokeSetLogin(true)
