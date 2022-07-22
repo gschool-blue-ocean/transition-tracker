a571d2dd25490a82a409896f598a2eb3e38039d3
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-22 15:39:51 -0500

    created and added modal

--- src/Components/StudentPage/components/SP-Tasks.jsx
+++ src/Components/StudentPage/components/SP-Tasks.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
-export default function SPTasks() {
+export default function SPTasks({ openModal }) {
    return (
       <div className="SDash--Tasks">
          <h4 id="StuTasks--Header">Tasks</h4>
-         <div className="StuTasks--Card">Task</div>
-         <div className="StuTasks--Card">Task</div>
-         <div className="StuTasks--Card">Task</div>
-         <div className="StuTasks--Card">Task</div>
-         <div className="StuTasks--Card">Task</div>
-         <div className="StuTasks--Card">Task</div>
+         <div className="StuTasks--Card" onClick={openModal}>
+            Task
+         </div>
+         <div className="StuTasks--Card" onClick={openModal}>
+            Task
+         </div>
+         <div className="StuTasks--Card" onClick={openModal}>
+            Task
+         </div>
+         <div className="StuTasks--Card" onClick={openModal}>
+            Task
+         </div>
+         <div className="StuTasks--Card" onClick={openModal}>
+            Task
+         </div>
+         <div className="StuTasks--Card" onClick={openModal}>
+            Task
+         </div>
       </div>
    );
 }
