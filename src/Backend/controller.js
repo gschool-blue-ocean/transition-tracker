84094d7f0dc80b517c94d22a239107b8b06402a1
Neo Temores <neo@Mac.lan1>
2022-07-22 15:30:55 -0700

    deploy

--- src/Backend/controller.js
+++ src/Backend/controller.js
@@ -1,13 +1,10 @@
 const pool = require('./connection.js');
-const csrf = require("csurf");
 const admin = require("firebase-admin")
 
 const serviceAccount = require("../../ServiceAccountKey.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
-
-const csrfMiddleware = csrf({ cookie: true })
 
 const cookiesForAll = async (req, res, next) => {
     res.cookie("XSRF-TOKEN", req.csrfToken())
