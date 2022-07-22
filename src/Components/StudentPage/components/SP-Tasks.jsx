6b25bee28505c5253617b436574f3123437d0f35
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-22 12:10:16 -0500

    grid layout of student page

--- src/Components/StudentPage/components/SP-Tasks.jsx
+++ src/Components/StudentPage/components/SP-Tasks.jsx
@@ -3,14 +3,13 @@ import React from "react";
 export default function SPTasks() {
    return (
       <div className="SDash--Tasks">
-         <h4>Tasks</h4>
-         <p>TASK</p>
-         <p>TASK</p>
-         <p>TASK</p>
-         <p>TASK</p>
-         <p>TASK</p>
-         <p>TASK</p>
-         <p>TASK</p>
+         <h4 id="StuTasks--Header">Tasks</h4>
+         <div className="StuTasks--Card">Task</div>
+         <div className="StuTasks--Card">Task</div>
+         <div className="StuTasks--Card">Task</div>
+         <div className="StuTasks--Card">Task</div>
+         <div className="StuTasks--Card">Task</div>
+         <div className="StuTasks--Card">Task</div>
       </div>
    );
 }
