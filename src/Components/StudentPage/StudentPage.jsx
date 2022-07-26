567ddb055e22999681688cf1bfd83496582a2583
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-26 14:06:38 -0500

    Changing notes to messaging

--- src/Components/StudentPage/StudentPage.jsx
+++ src/Components/StudentPage/StudentPage.jsx
@@ -131,7 +131,7 @@ export default function StudentPage() {
             <SPTaskModal />
          </Modal>
          <div className="SDash--Notes">
-            <label>Notes</label>
+            <label>Messaging</label>
             <textarea id="StuNotes--TextArea"></textarea>
          </div>
       </div>
