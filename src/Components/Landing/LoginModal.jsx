42f9ef8f799700ea07d344e5dcd8ec4e80f12b28
Neo Temores <neo@Mac.lan1>
2022-07-26 12:33:22 -0700

    deploy

--- src/Components/Landing/LoginModal.jsx
+++ src/Components/Landing/LoginModal.jsx
@@ -5,8 +5,8 @@ import AppContext from '../../Context/AppContext';
 import { useNavigate } from 'react-router-dom'
 import { auth, signInWithEmailAndPassword } from '../Firebase'
 import { app } from '../Firebase'
-import { CookiesProvider, withCookies, Cookies, useCookies } from 'react-cookie';
-
+// import { CookiesProvider, withCookies, Cookies, useCookies } from 'react-cookie';
+// import Cookies from 'js-cookie'
 
 function LoginModal({ invokeSetLogin, setShowLoginModal }) {
     const { allUsersData, allCohortsData } = useContext(AppContext)
@@ -18,65 +18,65 @@ function LoginModal({ invokeSetLogin, setShowLoginModal }) {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-
-        validateUserLoginData()
     }
 
-    let navigate = useNavigate()
-    const validateUserLoginData = () => {
-        signInWithEmailAndPassword(auth, loginData.username, loginData.password)
-            .then(({ user }) => {
-                user.getIdToken().then((idToken) => {
-                    fetch('https://hacking-transition.herokuapp.com/api/login', {
-                        method: "POST",
-                        headers: {
-                            Accept: "application/json",
-                            "Content-type": "application/json",
-                            "CSRF-Token": Cookies.get('XSRF-Token')
-                        },
-                        body: JSON.stringify({ idToken })
-                    })
 
-                })
-                    .then(() => app.auth().signOut())
-                    .then(() => invokeSetLogin(true))
+    // let navigate = useNavigate()
+    // const validateUserLoginData = () => {
+    //     signInWithEmailAndPassword(auth, loginData.username, loginData.password)
+    //         .then(({ user }) => {
+    //             // user.getIdToken().then(token => console.log(token))
+    //             user.getIdToken().then((idToken) => {
+    //                 fetch('https://hacking-transition.herokuapp.com/api/login', {
+    //                     method: "POST",
+    //                     headers: {
+    //                         Accept: "application/json",
+    //                         "Content-type": "application/json",
+    //                         "CSRF-Token": Cookies.get('_csrf')
+    //                     },
+    //                     body: JSON.stringify({ idToken })
+    //                 })
 
-                    .catch(err => console.error(err))
-            })
-        //         return fetch('https://hacking-transition.herokuapp.com/api/login', {
-        //             method: "POST",
-        //             headers: {
-        //                 Accept: "application/json",
-        //                 "Content-type": "application/json",
-        //                 "CSRF-Token": Cookies.get(user),
-        //             },
-        //             body: JSON.stringify({ idToken })
-        //         })
-        //     })
-        // })
-        // .then((data) => {
-        //     console.log(data)
-        //     // return app.auth().signOut();
-        // })
-        // .catch((err) => {
-        //     if (err) {
-        //         console.error(err)
-        //     }
-        // })
-        // .then(() => {
-        //     alert('Success')
-        // })
-        // allUsersData.forEach((elem) => {
-        //     if (loginData.username === elem.username && loginData.password === elem.password) {
-        //         elem.new_user ? navigate("/createAccount") : invokeSetLogin(true)
-        //         return console.log(elem)
-        //     }
-        // });
+    //             })
+    //                 // .then(() => app.auth().signOut())
+    //                 .then(() => invokeSetLogin(true))
+
+    //                 .catch(err => console.error(err))
+    //         })
+    //     //         return fetch('https://hacking-transition.herokuapp.com/api/login', {
+    //     //             method: "POST",
+    //     //             headers: {
+    //     //                 Accept: "application/json",
+    //     //                 "Content-type": "application/json",
+    //     //                 "CSRF-Token": Cookies.get(user),
+    //     //             },
+    //     //             body: JSON.stringify({ idToken })
+    //     //         })
+    //     //     })
+    //     // })
+    //     // .then((data) => {
+    //     //     console.log(data)
+    //     //     // return app.auth().signOut();
+    //     // })
+    //     // .catch((err) => {
+    //     //     if (err) {
+    //     //         console.error(err)
+    //     //     }
+    //     // })
+    //     // .then(() => {
+    //     //     alert('Success')
+    //     // })
+    //     // allUsersData.forEach((elem) => {
+    //     //     if (loginData.username === elem.username && loginData.password === elem.password) {
+    //     //         elem.new_user ? navigate("/createAccount") : invokeSetLogin(true)
+    //     //         return console.log(elem)
+    //     //     }
+    //     // });
 
 
-        // return console.warn('failed login attempt')
-        // invokeSetLogin(true)
-    }
+    //     // return console.warn('failed login attempt')
+    //     // invokeSetLogin(true)
+    // }
 
     const handleChange = (e) => {
         setLoginData((prevLoginData) => {
