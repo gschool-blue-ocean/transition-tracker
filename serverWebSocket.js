87cec77f698f93687fdcf93006967497eddbd1cf
Neo Temores <neo@Mac.lan1>
2022-07-27 15:22:59 -0700

    deploy

--- serverWebSocket.js
+++ serverWebSocket.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT + 1 || 8001;
 const cors = require("cors");
 const controller = require("./src/Backend/controller");
 
