e89d037864f9d79a8ce46d4f3fe196e8e5389f28
Neo Temores <neo@Mac.lan1>
2022-07-27 15:11:26 -0700

    web socket test

---
+++ serverWebSocket.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8001;
+const cors = require("cors");
+const controller = require("./src/Backend/controller");
+
+const http = require('http')
+const { Server } = require('socket.io');
+const server = http.createServer(app)
+
+app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ['GET', 'POST'],
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(PORT, 'web Socket running')
+})
