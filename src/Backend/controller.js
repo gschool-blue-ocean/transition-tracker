addfcf7823ad0d98499ed91675bf2f388ee26384
Neo Temores <neo@Mac.lan1>
2022-07-27 11:28:05 -0700

    deploy

--- src/Backend/controller.js
+++ src/Backend/controller.js
@@ -24,21 +24,21 @@ const testRoute = async (_, res) => {
 }
 
 //! ------------USER/ADMIN Table Logic------------
-// const hashAllPasswords = async (req, res) => {
-//     const { username, password } = req.body
-//     const hashedPassword = await bcrypt.hash(password, 10);
+const hashAllPasswords = async (req, res) => {
+    const { username, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-//     try {
-//         let client = await pool.connect()
-//         let data = await client.query('UPDATE users SET password = $1 WHERE username = $2 RETURNING *', [hashedPassword, username])
-//         res.json(data.rows)
-//         client.release()
+    try {
+        let client = await pool.connect()
+        let data = await client.query('UPDATE users SET password = $1 WHERE username = $2 RETURNING *', [hashedPassword, username])
+        res.json(data.rows)
+        client.release()
 
-//     } catch (error) {
-//         console.log(error)
-//         res.send(error)
-//     }
-// }
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+}
 
 const getAllUsers = async (_, res) => {
     try {
@@ -547,4 +547,5 @@ module.exports = {
     updateOneCommentByID,
     deleteOneCommentByID,
     login,
+    hashAllPasswords
 }
