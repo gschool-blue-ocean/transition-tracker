2b611be640a3328152abb44db6cc28d83b694154
Charles Stevens <cs-interface@protonmail.ch>
2022-07-28 15:08:29 +0000

    Implement editing cohort data

--- src/Components/AdminHomePage/EditCohortPage.jsx
+++ src/Components/AdminHomePage/EditCohortPage.jsx
@@ -1,27 +1,41 @@
-import {React, useContext} from 'react'
+import { React, useContext, useState } from 'react'
 import AppContext from "../../Context/AppContext"
+import EditableStudent from './EditableStudent'
+import EditableCohort from './EditableCohort'
+import { FiEdit } from 'react-icons/fi'
 
-function EditCohortPage ({ currentCohort }) {
+function EditCohortPage ({ selectedID }) {
   const { allUsersData, allCohortsData } = useContext(AppContext)
+  const [ students ] = useState({})
+
   return (
     <>
     {
       allCohortsData.map(x => {
-        if (x.cohort_id == currentCohort) {
+        if (x.cohort_id == selectedID) {
           return <>
-            <h1>{x.cohort_name}</h1>
-            
+            <EditableCohort
+              name={x.cohort_name}
+              start={x.start_date}
+              end={x.end_date}
+              id={selectedID}
+              />
             <ul>
               {
                 allUsersData.map(user => {
-                  if (user.cohort_id == currentCohort)
+                  if (user.cohort_id == selectedID)
                   return <>
-                    {user.first} {user.last}<br></br>
+                    <EditableStudent
+                      firstName={user.first}
+                      lastName={user.last}
+                      id={user.user_id}
+                      cohort={selectedID}/>
+                    <br></br>
                   </>
                 })
               }
             </ul>
-            <button>save changes</button>
+            { allUsersData.filter(user => user.cohort_id == selectedID).length } students
           </>
         }
       })
