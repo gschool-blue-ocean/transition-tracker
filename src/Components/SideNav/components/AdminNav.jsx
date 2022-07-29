c79869778cd5005053ead1b139fe67f0a02700a5
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-29 10:06:47 -0500

    prep for merging

--- src/Components/SideNav/components/AdminNav.jsx
+++ src/Components/SideNav/components/AdminNav.jsx
@@ -1,12 +1,56 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../../../Context/AppContext";
+import Loading from "../../LoadingDisplay/Loading";
 
 export default function AdminNav() {
-   const { allUsersData } = useContext(AppContext);
-   return (
-      <div className="sideNav">
-         <h3>Cohorts</h3>
-         <div>List</div>
-      </div>
-   );
+   const { allUsersData, setLoading } = useContext(AppContext);
+   const [cohortStudents, setCohortStudents] = useState(null);
+   const [activeStudent, setActiveStudent] = useState(null);
+
+   let id = 1;
+
+   useEffect(() => {
+      getStudentList(id);
+   }, []);
+
+   const handleClick = (e) => {
+      setActiveStudent(e.target.id);
+      e.target.classList.toggle("activeStudent");
+   };
+
+   const getStudentList = (id) => {
+      fetch(`https://hacking-transition.herokuapp.com/api/users/cohort/${id}`)
+         .then((res) => res.json())
+         .then((list) => setCohortStudents(list));
+   };
+
+   console.log(cohortStudents);
+
+   if (!cohortStudents) {
+      setLoading(true);
+      return <Loading />;
+   } else {
+      setLoading(false);
+      return (
+         <div className="sideNav">
+            <h3>MSCP-12</h3>
+            <div>
+               {cohortStudents.map((student) => {
+                  return (
+                     <div>
+                        <button
+                           className={"sideNav--StudentBtn"}
+                           id={student.user_id}
+                           key={student.user_id}
+                           onClick={handleClick}
+                        >
+                           {student.first} {student.last}
+                        </button>
+                     </div>
+                  );
+               })}
+            </div>
+         </div>
+      );
+   }
 }
