62e82d8eb6204533527c774bce1dbde92997df02
magdielrosario97 <magdielrosario97@gmail.com>
2022-08-09 11:26:09 -0500

    saving working code

--- src/Components/StudentPage/components/SP-Tasks.jsx
+++ src/Components/StudentPage/components/SP-Tasks.jsx
@@ -2,37 +2,39 @@ import React, { useEffect, useState } from "react";
 import Loading from "../../LoadingDisplay/Loading";
 
 export default function SPTasks({ openModal, activeStudent }) {
-   // const [selectedStudent, setSelectedStudent] = useState(activeStudent.user_id);
-   // const [studentTasks, setStudentTasks] = useState([]);
-   // useEffect(() => {
-   //    setSelectedStudent(activeStudent.user_id);
-   //    fetch(`https://hacking-transition.herokuapp.com/api/tasks/student/${activeStudent.user_id}`)
-   //       .then((res) => res.json())
-   //       .then((tasks) => {
-   //          console.log(tasks);
-   //          setStudentTasks(tasks);
-   //       });
-   // }, [activeStudent]);
-   // if (!studentTasks) {
-   //    return <Loading />;
-   // } else {
-   //    return (
-   //       <div className="SDash--Tasks">
-   //          <h4 id="StuTasks--Header">Tasks</h4>
-   //          {/* <>
-   //             {studentTasks.length === 0 ? (
-   //                <div>{activeStudent.first} has not started a task</div>
-   //             ) : (
-   //                studentTasks.map((task) => {
-   //                   return (
-   //                      <div className="StuTasks--Card" id={task.task_id} key={task.task_id} onClick={openModal}>
-   //                         {task.title}
-   //                      </div>
-   //                   );
-   //                })
-   //             )}
-   //          </> */}
-   //       </div>
-   //    );
-   // }
+   const [studentTasks, setStudentTasks] = useState([]);
+   const [loading, setLoading] = useState(true);
+   useEffect(() => {
+      if (activeStudent.user_id) {
+         fetch(`https://hacking-transition.herokuapp.com/api/tasks/student/${activeStudent.user_id}`)
+            .then((res) => res.json())
+            .then((tasks) => {
+               setLoading(false);
+               setStudentTasks(tasks);
+            });
+      }
+   }, [activeStudent]);
+
+   if (loading) {
+      return <Loading />;
+   } else {
+      return (
+         <div className="SDash--Tasks">
+            <h4 id="StuTasks--Header">Tasks</h4>
+            <>
+               {studentTasks.length === 0 ? (
+                  <div>{activeStudent.first} has not started a task</div>
+               ) : (
+                  studentTasks.map((task) => {
+                     return (
+                        <div className="StuTasks--Card" id={task.task_id} key={task.task_id} onClick={openModal}>
+                           {task.title}
+                        </div>
+                     );
+                  })
+               )}
+            </>
+         </div>
+      );
+   }
 }
