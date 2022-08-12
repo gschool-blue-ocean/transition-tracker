6034ddfe1cc75e64eb51067b8ca95a3a17257795
AnthonyWright17 <awright032317@gmail.com>
2022-08-11 18:22:42 -0600

    CSS for Settings page

--- src/Components/Settings/AdminListItems.jsx
+++ src/Components/Settings/AdminListItems.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import DeleteAdminButton from './DeleteAdminButton';
+import '../../StyleSheets/Settings.css'
 
 export const AdminListItems = ({user, key}) => {
   console.log(user.user_id)
   return (
     <div className='admin--listItems' id={user.user_id}>
-      {user.first} {user.last} <DeleteAdminButton id={user.user_id} key={user.user_id} />
+
+      <span className='adminList--name'>
+      {user.first} {user.last} 
+      </span>
+
+      <span className='adminList--deleteBtn'>
+      <DeleteAdminButton id={user.user_id} key={user.user_id} />
+      </span>
+
     </div>
   )
 }
